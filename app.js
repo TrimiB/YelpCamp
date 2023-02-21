f8d665c06ac4318647e0e104a38b9a96281cce72
Kushtrim <bujupikushtrim@outlook.com>
2023-02-21 13:27:52 +0100

    Creted a new route '/campgrounds' and a file with the 'to be rendered ejs file'. Also tested rendered file by passing in all campground documents from db

--- app.js
+++ app.js
@@ -26,13 +26,9 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({
-    title: 'My backyard',
-    description: 'Cheap Camping!',
-  });
-  await camp.save();
-  res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
 });
 
 app.listen(3000, () => {
