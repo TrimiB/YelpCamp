232440de2cb09c4a5ac452e08601e3d3455f9a00
Kushtrim <bujupikushtrim@outlook.com>
2023-02-27 12:17:36 +0100

    Added basic Bootstrap Styles on some Pages

--- seeds/index.js
+++ seeds/index.js
@@ -39,16 +39,18 @@ const seedDB = async () => {
           cities[random1000].latitude,
         ],
       },
-      images: [
-        {
-          url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-          filename: 'YelpCamp/ahfnenvca4tha00h2ubt',
-        },
-        {
-          url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-          filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi',
-        },
-      ],
+      image:
+        'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+      // images: [
+      //   {
+      //     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+      //     filename: 'YelpCamp/ahfnenvca4tha00h2ubt',
+      //   },
+      //   {
+      //     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+      //     filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi',
+      //   },
+      // ],
     });
     await camp.save();
   }
