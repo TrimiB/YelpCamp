46c38472864c00ff6aee40476d29d9324ae22473
Kushtrim <bujupikushtrim@outlook.com>
2023-11-06 23:55:28 +0100

    created a middleware to store the original url.

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ passport.deserializeUser(User.deserializeUser());
 // Making the flash message "success" available in the WHOLE! templates.
 // Message comes from creating new Campgroung.
 app.use((req, res, next) => {
+  console.log(req.session);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { storeReturnTo } = require('../middleware');
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
@@ -33,10 +34,15 @@ router.get('/login', (req, res) => {
 
 router.post(
   '/login',
+  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+  storeReturnTo,
+  // passport.authenticate logs the user in and clears req.session
   passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+  // Now we can use res.locals.returnTo to redirect the user after login
   (req, res) => {
-    req.flash('success', 'Welcome Back');
-    res.redirect('/campgrounds');
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+    res.redirect(redirectUrl);
   }
 );
 
