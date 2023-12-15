ff81c597a30c0a677eb525c5555aa27b6aa393b6
Kushtrim <bujupikushtrim@outlook.com>
2023-12-15 22:20:53 +0100

    Adding DOTENV and Multer to the code

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,12 +6,28 @@ const campground = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${file.originalname.split('.')[0]}-${uniqueSuffix}.${file.mimetype.split('/')[1]}`);
+  },
+});
+const upload = multer({ storage: storage });
+
 router
   .route('/')
   // Get all campgrounds
   .get(catchAsync(campground.index))
   // Creating a new campground
-  .post(isLoggedIn, validateCampground, catchAsync(campground.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campground.createCampground));
+  .post(upload.array('image', 4), (req, res) => {
+    console.log(req.body, req.files);
+    res.send(`It worked! 👌`);
+  });
 
 // Get New campground form [Order matters on this line]
 router.get('/new', isLoggedIn, campground.renderNewForm);
