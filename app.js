f4eb9fc5e403e990372002fcd630b587995bd3bb
Kushtrim <bujupikushtrim@outlook.com>
2023-12-30 22:52:59 +0100

    Minor changes to the cookie sessions options

--- app.js
+++ app.js
@@ -59,11 +59,13 @@ app.use(mongoSanitize());
 
 // created as session "cookie".
 const sessionCongif = {
+  name: 'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
